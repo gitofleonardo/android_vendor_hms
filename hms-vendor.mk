84ecc774364ecd43c8d2f34dcae552743f5afc87
hhvvg <hvangchengxi@outlook.com>
2023-10-15 18:56:06 +0800

    Init commit

---
+++ hms-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/hms
+
+PRODUCT_PACKAGES += \
+    HMS_Core \
+    AppGallery \
+    hms_core_whitelist.xml \
+    privapp-permissions-hms_core.xml \
+    default_permissions_com.huawei.hms.xml \
+    default_permissions_com.huawei.appmarket.xml
