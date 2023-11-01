5924c3cf83d67f1dc53a79901ae59e4db1f5964b
hhvvg <hvangchengxi@outlook.com>
2023-11-01 20:07:20 +0800

    Split build targets

--- hms-vendor.mk
+++ hms-vendor.mk
@@ -1,13 +1,19 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/hms
 
-PRODUCT_PACKAGES += \
-    HMS_Core \
-    AppGallery \
-    hms_core_whitelist.xml \
-    privapp-permissions-hms_core.xml \
-    default_permissions_com.huawei.hms.xml \
-    default_permissions_com.huawei.appmarket.xml
+ifeq ($(TARGET_INCLUDE_HMS),true)
+    PRODUCT_PACKAGES += \
+        HMS_Core \
+        hms_core_whitelist.xml \
+        privapp-permissions-hms_core.xml \
+        default_permissions_com.huawei.hms.xml
+endif
+
+ifeq ($(TARGET_INCLUDE_APP_GALLERY),true)
+    PRODUCT_PACKAGES += \
+        AppGallery \
+        default_permissions_com.huawei.appmarket.xml
+endif
 
 ifeq ($(TARGET_INCLUDE_AURORA),true)
 	PRODUCT_PACKAGES += \
